53303dd8b0167170310199c6700e9f9880865ad4
alex <strim0125@yandex.ru>
2021-11-25 19:37:11 +0300

    1.1

--- src/ru/geekbrains/common/part2_lesson4/Main.java
+++ src/ru/geekbrains/common/part2_lesson4/Main.java
@@ -1,11 +1,23 @@
 package ru.geekbrains.common.part2_lesson4;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello !!!");
+        // write your code here
+        masInit();
     }
 
-    public static void
+    public static void masInit() {
+            int[] myArray = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+            int size = myArray.length;
+            for (int i = 0; i < size / 2; i++) {
+                int temp = myArray[i];
+                myArray[i] = myArray[size - 1 - i];
+                myArray[size - 1 - i] = temp;
+            }
+            System.out.println("Array after reverse:: ");
+            System.out.println(Arrays.toString(myArray));
+        }
 }
