cd46f25dbd43586f7644ee5a99607457fbe77a4b
alex <strim0125@yandex.ru>
2021-11-28 19:46:51 +0300

    5

--- src/ru/geekbrains/common/part2_lesson4/Main.java
+++ src/ru/geekbrains/common/part2_lesson4/Main.java
@@ -1,28 +1,34 @@
 package ru.geekbrains.common.part2_lesson4;
 
+import org.omg.PortableInterceptor.SUCCESSFUL;
+
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
         masIn();
         massiv();
         masMas();
+        massiveQ(5);
+        int[] arr = {2, 2, 2, 1, 2, 2, 10, 1};
+        System.out.println(checkBalance(arr));
+
     }
+    
 
     public static void masIn() {
-            int[] myArr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
-            int size = myArr.length;
-            for (int i = 0; i < size / 2; i++) {
-                int temp = myArr[i];
-                myArr[i] = myArr[size - 1 - i];
-                myArr[size - 1 - i] = temp;
-            }
-            System.out.println(Arrays.toString(myArr));
+        int[] myArr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int size = myArr.length;
+        for (int i = 0; i < size / 2; i++) {
+            int temp = myArr[i];
+            myArr[i] = myArr[size - 1 - i];
+            myArr[size - 1 - i] = temp;
         }
+        System.out.println(Arrays.toString(myArr));
+    }
 
-    public static void massiv(){
+    public static void massiv() {
         int[] intMass = new int[100];
         for (int i = 0; i < 100; i++) {
             intMass[i] = i + 1;
@@ -30,8 +36,8 @@ public class Main {
         System.out.println(Arrays.toString(intMass));
     }
 
-    public static void masMas(){
-        int[] masInt = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    public static void masMas() {
+        int[] masInt = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < masInt.length; i++) {
             if (masInt[i] < 6) {
                 masInt[i] *= 2;
@@ -40,4 +46,33 @@ public class Main {
         System.out.println(Arrays.toString(masInt));
     }
 
+    public static int[][] massiveQ(int n) {
+        int[][] mas = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    mas[i][j] = 1;
+                    System.out.print(" " + mas[i][j] );
+                }
+             }
+        }
+        return mas;
+    }
+
+    public static boolean checkBalance(int[] arr) {
+        int summa = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summa += arr[i];
+        }
+        if (summa % 2 != 0){
+            return false;
+        }
+        int summLeft = 0;
+        int i = 0;
+        while (summLeft < summa / 2) {
+            summLeft += arr[i];
+            i++;
+        }
+        return summLeft == summa / 2;
+        }
 }
